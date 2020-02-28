5c031f868ad174f08f64f0375d2d1303bd40f29e
JulNobel <32543881+JulNobel@users.noreply.github.com>
2020-02-28 14:15:40 +0300

    Update node.py

--- week_02_exceptions/node.py
+++ week_02_exceptions/node.py
@@ -1,9 +1,14 @@
 class Node:
+    _next_id = 0
     def __init__(self, label=''):
-        pass
+        self.id = _next_id
+        # self.count += 1
+        # pass
 
     def __str__(self):
         raise NotImplementedError
     
     def __hash__(self):
         raise NotImplementedError
+n1 = Node()
+n2 = Node()
