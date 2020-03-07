1cac559ba602944cd8d0115fcd5d992f478df5a4
JulNobel <32543881+JulNobel@users.noreply.github.com>
2020-03-07 15:18:33 +0300

    Revert "Merge branch 'master' of https://github.com/JulNobel/misis-py-adv"
    This reverts commit 4c54dad0fb4ea0607cb502e2cf5f53c455094c9e, reversing
    changes made to 5c031f868ad174f08f64f0375d2d1303bd40f29e.

--- week_02_exceptions/graph.py
+++ week_02_exceptions/graph.py
@@ -2,44 +2,19 @@ from node import Node
 
 
 class Graph:
-    def __init__(self, adj: dict):
-        self.adj = adj
+    def __init__(self):
+        pass
 
     @classmethod
-    def from_dict(cls, input_dict: dict):
-        d = input_dict.copy()
-        for node, links in input_dict.items():
-            for link in links:
-                if link not in d:
-                    d[link] = [node]
-                else:
-                    if node not in d[link]:
-                        d[link].append(node)
-        return cls(d)
-    
-    def __str__(self):
-        return '<Graph {}>'.format(self.adj)
+    def from_adjacency_map(self, adj: dict):
+        raise NotImplementedError
 
     def in_graph(self, node: Node) -> bool:
         raise NotImplementedError
 
-    def add_node(self, node: Node):
-        raise NotImplementedError
-    
-    def add_edge(self, node1: Node, node2: Node):
+    def add_node(self, node: Node, links: list):
         raise NotImplementedError
 
 
 class WeightedGraph(Graph):
     pass
-
-
-if __name__ == "__main__":
-    n1, n2, n3 = Node(), Node(), Node()
-    d = {
-        n1: [n2],
-        n2: [n3]
-    }
-    print(d)
-    g = Graph.from_dict(d)
-    print(g)
--- week_02_exceptions/main.py
+++ week_02_exceptions/main.py
@@ -6,9 +6,8 @@ if __name__ == "__main__":
     n1, n2, n3 =  Node(), Node(), Node()
     adj = {
         n1: [n2, n3],
-        n2: [n3]
+        n2: [n3, n1],
+        n3: [n2, n1]
     }
 
-    g = Graph.from_dict(adj)
-    print(g)
-
+    g = Graph.from_adjacency_map(adj)
